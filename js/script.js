51303c430b18cba27e88d3c87013bc3cc7fb7c8d
dangcongthangcv3 <dangcongthangcv3@gmailcom>
2024-02-15 15:19:52 +0700

    fix

--- js/script.js
+++ js/script.js
@@ -10,7 +10,9 @@ window.onscroll = () => {
 }
 
 // HOME
+// Vòng lặp và xử lý sự kiện click vào hình ảnh
 document.querySelectorAll('.image-slider img').forEach(images => {
+    // click và thay đổi nguồn
     images.onclick = () => {
         var src = images.getAttribute('src');
         document.querySelector('.main-home-image').src = src;
@@ -19,15 +21,19 @@ document.querySelectorAll('.image-slider img').forEach(images => {
 
 // REVIEW
 var swiper = new Swiper(".review-slider", {
+    // khoảng cách giữa các slide
     spaceBetween: 20,
+    // thanh phân trang
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
     },
+    // tự động trượt
     autoplay: {
         delay: 7500,
         disableOnInteraction: false,
     },
+    // số lượng slide hiển thị trên mỗi trang
     breakpoints: {
         0: {
             slidesPerView: 0,
@@ -36,7 +42,9 @@ var swiper = new Swiper(".review-slider", {
             slidesPerView: 2,
         },
     },
+    // vòng lặp vô hạn 
     loop: true,
+    // hình dạng của con trỏ chuột
     grabCursor: true,
 });
 // CLOSED REVIEW
